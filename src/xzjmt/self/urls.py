f7ad77cb1aa32a04a330efd6e526683242eaf8b7
shurrik <darkforest123@gmail.com>
2013-06-18 16:14:18 +0800

    self方法

--- src/xzjmt/self/urls.py
+++ src/xzjmt/self/urls.py
@@ -2,4 +2,18 @@ from django.conf.urls.defaults import *
 import xzjmt.self.views
 urlpatterns = patterns('',
     url(r'^$', xzjmt.self.views.index),
+    url(r'^item/new$', xzjmt.self.views.newItem),
+    url(r'^item/list$', xzjmt.self.views.listItem),
+    url(r'^item/del/(?P<id>\d)$', xzjmt.self.views.delItem),
+    url(r'^item/add$', xzjmt.self.views.addItem),
+    url(r'^profile$', xzjmt.self.views.profile),
+    url(r'^save$', xzjmt.self.views.save),
+    url(r'^message/write/(?P<id>\d)$', xzjmt.self.views.write),
+    url(r'^message/send$', xzjmt.self.views.send),
+    url(r'^message/list$', xzjmt.self.views.listMessage),
+    url(r'^message/(?P<id>\d)$', xzjmt.self.views.messageDetail),
+    url(r'^message/reply/(?P<id>\d)$', xzjmt.self.views.replyMessage),
+    url(r'^follow/(?P<id>\d)$', xzjmt.self.views.follow),
+    url(r'^collect/(?P<id>\d)$', xzjmt.self.views.collect),
+    url(r'^collection$', xzjmt.self.views.collection),
 )
--- src/xzjmt/self/views.py
+++ src/xzjmt/self/views.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
@@ -9,3 +10,87 @@ def index(request):
     c = RequestContext(request, {
     })
     return render_to_response('self/index.html',c)
+
+@login_required
+def newItem(request):
+    c = RequestContext(request, {
+    })
+    return render_to_response('self/item.html',c)
+
+@login_required
+def listItem(request):
+    c = RequestContext(request, {
+    })
+    return render_to_response('self/item_list.html',c)
+
+@login_required
+def delItem(request,id):
+    c = RequestContext(request, {
+    })
+    return HttpResponseRedirect("/self/item/list")
+
+@login_required
+def addItem(request):
+    c = RequestContext(request, {
+    })
+    return HttpResponseRedirect("/self")
+
+@login_required
+def profile(request):
+    c = RequestContext(request, {
+    })
+    return render_to_response('self/profile.html',c)
+
+@login_required
+def save(request):
+    c = RequestContext(request, {
+    })
+    return HttpResponseRedirect("/self")
+
+@login_required
+def write(request,usrId):
+    c = RequestContext(request, {
+    })
+    return render_to_response('self/message/write.html',c)
+
+@login_required
+def send(request):
+    c = RequestContext(request, {
+    })
+    return HttpResponseRedirect("/self")
+
+@login_required
+def listMessage(request):
+    c = RequestContext(request, {
+    })
+    return render_to_response('self/message/list.html',c)
+
+@login_required
+def messageDetail(request,id):
+    c = RequestContext(request, {
+    })
+    return render_to_response('self/message/detail.html',c)
+
+@login_required
+def replyMessage(request,id):
+    c = RequestContext(request, {
+    })
+    return render_to_response('self/message/reply.html',c)
+
+@login_required
+def follow(request,usrId):
+    c = RequestContext(request, {
+    })
+    return 'succ'
+
+@login_required
+def collect(request,itemId):
+    c = RequestContext(request, {
+    })
+    return 'succ'
+
+@login_required
+def collection(request):
+    c = RequestContext(request, {
+    })
+    return render_to_response('self/collection.html',c)
